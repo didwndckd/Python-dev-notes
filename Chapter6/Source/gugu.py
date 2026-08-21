90031c6c1a167a69eede825af393f9989967829f
didwndckd <didwndckd@gmail.com>
2026-08-21 19:29:59 +0900

    gugu.py 수정

--- Chapter6/Source/gugu.py
+++ Chapter6/Source/gugu.py
@@ -1,17 +1,19 @@
 # 구구단 함수 만들기
 
-# def gugu(n):
-#     result = []
-#     result.append(n*1)
-#     result.append(n*2)
-#     result.append(n*3)
-#     result.append(n*4)
-#     result.append(n*5)
-#     result.append(n*6)
-#     result.append(n*7)
-#     result.append(n*8)
-#     result.append(n*9)
-#     return result
+def gugu(n):
+    result = []
+    result.append(n*1)
+    result.append(n*2)
+    result.append(n*3)
+    result.append(n*4)
+    result.append(n*5)
+    result.append(n*6)
+    result.append(n*7)
+    result.append(n*8)
+    result.append(n*9)
+    return result
+
+print(gugu(2))
 
 def gugu(n):
     result = []
@@ -21,5 +23,12 @@ def gugu(n):
         i += 1
     return result
 
-result = gugu(2)
-print(result)
+print(gugu(2))
+
+def gugu(n):
+    result = []
+    for i in range(1, 10):
+        result.append(n * i)
+    return result
+
+print(gugu(2))
